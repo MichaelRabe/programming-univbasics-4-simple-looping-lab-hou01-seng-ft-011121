604c702c90be47c9b2bf2d7f26e196c03e33b18d
Michael Rabe <m.trabe96@gmail.com>
2020-12-17 20:13:09 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -5,3 +5,12 @@ def loop_message_five_times(message="Hello World")
     count +=1
   end
 end
+
+def loop_message_n_times(message="Hello Moon", n = 5)
+  count = 0
+  while count < n do
+    puts message
+    count +=1
+  end
+end
+  
